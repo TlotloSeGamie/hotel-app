d860b2ad1854bacafb7cfdae3aaf05941ea35738
TlotloSeGamie <t88segaamie@gmail.com>
2024-09-13 12:35:04 +0200

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="address">
+
+            </div>
+            <div className="contact">
+
+            </div>
+            <div className="map">
+
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
