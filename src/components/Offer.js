f0ee6595d1893ef3398fe686ad34a22ed7b548ad
TlotloSeGamie <t88segamie@gmail.com>
2024-09-13 03:39:39 +0200

    second commit

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,12 +1,16 @@
-import "./Offer.css"
-import scndfloor from "../images/202209132306370.lagoon_b.png"; 
+import "./Offer.css";
+import scndfloor from "../images/202209132306370.lagoon_b.png";
+import food from "../images/food icon.jpg";
+import services from "../images/service icon.jpg";
+import spa from "../images/spa icon.jpg";
+import relax from "../images/icon chair.jpg";
 
 const Offer = () => {
     return (
         <div className="offer-container">
             <div className="top">
                 <div className="image">
-                    <img src={scndfloor} />
+                    <img src={scndfloor} alt="Scenic view" />
                 </div>
                 <div className="text">
                     <h2><u>Your escape to luxury</u></h2>
@@ -17,40 +21,44 @@ const Offer = () => {
                 </div>
             </div>
             <div className="middle">
-                <h4><span>What do we offer</span> <br></br>There are many variations of passages  
-                </h4>
-                
+                <h4><span>What do we offer</span><br />There are many variations of passages</h4>
             </div>
             <div className="bottom">
                 <div className="confortable">
+                    <img src={relax} alt="Comfortable rest" />
                     <h5><u>Comfortable rest</u>
-                        <br></br>
-                            At Da Gamie Hotel & Resort, relaxation is key. Our spacious rooms, with plush bedding and serene décor, offer a peaceful retreat. Enjoy restful nights
-                            and wake up refreshed, or unwind outdoors in our tranquil gardens and poolside lounges.</h5>
+                        <br />
+                        At Da Gamie Hotel & Resort, relaxation is key. Our spacious rooms, with plush bedding and serene décor, offer a peaceful retreat. Enjoy restful nights
+                        and wake up refreshed, or unwind outdoors in our tranquil gardens and poolside lounges.
+                    </h5>
                 </div>
                 <div className="spa">
+                    <img src={spa} alt="Spa Treatments" />
                     <h5><u>Spa Treatments</u>
-                        <br></br>
+                        <br />
                         Relax and rejuvenate with soothing spa treatments at Da Gamie. Our expert therapists offer massages,
-                        facials, and holistic therapies designed to refresh your body and mind.</h5>
+                        facials, and holistic therapies designed to refresh your body and mind.
+                    </h5>
                 </div>
                 <div className="food">
+                    <img src={food} alt="Delicious Food" />
                     <h5><u>Delicious Food</u>
-                        <br></br>
+                        <br />
                         Indulge in delicious, locally-sourced meals at Da Gamie. From gourmet fine dining to casual bites, our restaurants 
-                        offer diverse, flavorful dishes that cater to every taste.</h5>
+                        offer diverse, flavorful dishes that cater to every taste.
+                    </h5>
                 </div>
                 <div className="best">
-                   <h5>
-                        <u>Best Service</u>
-                            <br></br>
-                            We pride ourselves on providing exceptional service at Da Gamie. Our attentive staff ensures every detail is handled, offering
-                            personalized care to make your stay seamless and enjoyable.
+                   <img src={services} alt="Best Service" />
+                    <h5><u>Best Service</u>
+                        <br />
+                        We pride ourselves on providing exceptional service at Da Gamie. Our attentive staff ensures every detail is handled, offering
+                        personalized care to make your stay seamless and enjoyable.
                     </h5>
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default Offer;
